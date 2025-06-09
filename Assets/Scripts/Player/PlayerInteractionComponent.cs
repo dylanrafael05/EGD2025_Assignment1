4553bf5827bc10d4a4d1bbae4a3b960254e32dfd
MEKNOWKNOOB <149892791+MEKNOWKNOOB@users.noreply.github.com>
2025-06-09 17:22:34 -0400

    Basic Inventory and Item System

--- Assets/Scripts/Player/PlayerInteractionComponent.cs
+++ Assets/Scripts/Player/PlayerInteractionComponent.cs
@@ -1,15 +1,18 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerInteractionComponent : MonoBehaviour
 {
     [NonSerialized] private BoxCollider boxCollider;
+    [NonSerialized] private PlayerInventoryComponent playerInventoryComponent;
 
 
 
     void Awake()
     {
         boxCollider = GetComponent<BoxCollider>();
+        playerInventoryComponent = GetComponent<PlayerInventoryComponent>();
     }
 
     private void OnTriggerStay(Collider other)
@@ -19,15 +22,31 @@ public class PlayerInteractionComponent : MonoBehaviour
             return;
         }
 
-        if (other.gameObject.layer == 8)
+        if (other.gameObject.layer != 8)
         {
-            if (other.CompareTag("Tree") && InputManager.instance.interactBool)
+            return;
+        }
+                    
+        if (other.CompareTag("Tree"))
+        {
+            if (InputManager.instance.interactBool)
             {
-                print("hallo");
+                playerInventoryComponent.ObtainFireWood();
+                other.gameObject.transform.parent.parent.gameObject.GetComponent<TreeProp>().ChopDown();
             }
-            else if (false)
+        }
+        else if (other.CompareTag("Item"))
+        {
+            if (InputManager.instance.interactBool)
             {
-
+                playerInventoryComponent.ReplaceItem(other.gameObject);
+            }
+        }
+        else if (other.CompareTag("CampFire"))
+        {
+            if (InputManager.instance.interactBool)
+            {
+                playerInventoryComponent.BurnInventory();
             }
         }
     }
