b810bcd2a4daaa03544f4e6ead3d585c749a8cac
MEKNOWKNOOB <149892791+MEKNOWKNOOB@users.noreply.github.com>
2025-06-09 18:05:14 -0400

    Basic Implementation for CampFire

---
+++ Assets/Scripts/Props/CampFireManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CampFireManager : MonoBehaviour
+{
+
+
+
+    public void IncreaseBurn(int totalValue)
+    {
+        print("put in burnvalue = " + totalValue);
+    }
+}
